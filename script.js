11a2d07d16790b4e17f4ec972c2b37a521794758
lisaruwona <lisafaithruwona@gmail.com>
2025-02-20 14:47:45 +0000

    commiting changes

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const timerPage = document.querySelector('.timer-section');
-
 timerPage.addEventListener('click', function(){
     window.location.href = 'timer.html';
 })
+
+
+const runnerPage = document.querySelector('.runner-id-section');
+runnerPage.addEventListener('click', function(){
+    window.location.href = 'registerRunner.html';
+})
+ 
