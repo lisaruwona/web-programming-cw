ad7d3af87498a516fc5a8e38935bd69e230d1a2d
lisaruwona <lisafaithruwona@gmail.com>
2025-02-25 17:04:24 +0000

    commiting changes

--- frontend/script.js
+++ frontend/script.js
@@ -3,9 +3,35 @@ timerPage.addEventListener('click', function(){
     window.location.href = 'timer.html';
 })
 
+const display = document.getElementById('display');
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function go() {
+    if (!isRunning) {
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        isRunning = true;
+    }
+}
+
+function update() {
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let count = Math.floor(elapsedTime % 1000 / 10);
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${count}`;
+}
 
 const runnerPage = document.querySelector('.runner-id-section');
 runnerPage.addEventListener('click', function(){
     window.location.href = 'registerRunner.html';
 })
- 
+
+
